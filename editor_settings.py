a4cc5359dd3eb60fda9767e2f28ce408b013b6ea
FoxCunning <naprossen@gmail.com>
2021-01-30 09:07:21 +0000

    Now uses configparser

--- editor_settings.py
+++ editor_settings.py
@@ -1,10 +1,12 @@
 __author__ = "Fox Cunning"
 
+import configparser
 import os
 from dataclasses import dataclass
 
-
 # ----------------------------------------------------------------------------------------------------------------------
+from typing import Union
+
 from debug import log
 
 
@@ -13,19 +15,25 @@ class EditorSettings:
     """
     A fairly generic class for keeping track of settings, loading/saving etc.
     """
-    settings_file: str = "settings.conf"
-    _keys = {"last rom path": "",
-             "make backups": True,
-             "emulator": "",
-             "emulator parameters": "%f",
-             "last map import path": "",
-             "last map export path": "",
-             "editor fonts": "Consolas",
-             "sync npc sprites": True,
-             "fix envelope bug": True,
-             "sample rate": 44100,
-             "audio host": "directsound"
-             }
+    SETTINGS_FILE: str = "settings.conf"
+    config: configparser.ConfigParser = configparser.ConfigParser()
+    _path: str = ""
+
+    KEYS = {"last rom path": "",
+            "make backups": True,
+            "close sub-window after saving": False,
+            "emulator": "",
+            "emulator parameters": "%%f",
+            "last map import path": "",
+            "last map export path": "",
+            "last music import path": "",
+            "last music export path": "",
+            "editor fonts": "Consolas",
+            "sync npc sprites": True,
+            "fix envelope bug": True,
+            "sample rate": 44100,
+            "audio host": "directsound"
+            }
 
     # ------------------------------------------------------------------------------------------------------------------
 
@@ -40,30 +48,29 @@ class EditorSettings:
             True if settings have been successfully loaded. False otherwise.
         """
         # Get execution path
-        path: str = os.path.realpath(__file__)
+        self._path: str = os.path.realpath(__file__)
         try:
-            last = path.rindex('\\')
+            last = self._path.rindex('\\')
         except ValueError:
             try:
-                last = path.rindex('/')
+                last = self._path.rindex('/')
             except ValueError:
                 # Does not look like a directory name
                 return False
-        path = path[:last + 1]
-        path = path + self.settings_file
+        self._path = self._path[:last + 1]
+        self._path = self._path + EditorSettings.SETTINGS_FILE
+
         try:
-            file = open(path, "rt")
-        except OSError:
-            log(3, "EDITOR", "Could not load settings from file. Creating one.")
-            return self.save()
+            self.config.read(self._path)
+        except configparser.ParsingError:
+            log(3, "EditorSettings", "Could not load settings from file. A default one will be created.")
 
-        # Read and parse lines
-        line = file.readline()
-        while line != "":
-            self._parse_line(line)  # Parse last line
-            line = file.readline()  # Get next line
+            self.config.add_section("SETTINGS")
+            for k in EditorSettings.KEYS:
+                self.config.set("SETTINGS", k, EditorSettings.KEYS.get(k))
 
-        file.close()
+            self.config.write(open(self._path, "w"))
+
         return True
 
     # ------------------------------------------------------------------------------------------------------------------
@@ -77,114 +84,30 @@ class EditorSettings:
         bool
             True if settings were successfully saved. False otherwise.
         """
-        # Get execution path
-        path: str = os.path.realpath(__file__)
-        try:
-            last = path.rindex('\\')
-        except ValueError:
-            try:
-                last = path.rindex('/')
-            except ValueError:
-                # Does not look like a directory name
-                return False
-        path = path[:last + 1]
-
-        path = path + self.settings_file
+        success = True
 
         try:
-            file = open(path, "wt")
-        except OSError:
-            log(3, "EDITOR", "Could not save settings to file. Make sure you have write access to the editor's folder.")
-            return False
+            self.config.write(open(self._path, "w"))
 
-        for key in self._keys:
-            setting_string = f"{key} = {self._keys[key]}\n"
-            file.write(setting_string)
+        except IOError as error:
+            log(2, "EditorSettings", f"Error saving configuration file: '{error}'.")
+            success = False
 
-        file.close()
-        return True
+        return success
 
     # ------------------------------------------------------------------------------------------------------------------
 
-    def _parse_line(self, line: str) -> None:
-        """
-        Parses one line of text from the settings file
+    def get(self, key: str) -> Union[int, bool, str]:
+        if (key == "make backups" or key[:8] == "sync npc" or key[:12] == "fix envelope" or
+                key[:16] == "close sub-window"):
+            return self.config.getboolean("SETTINGS", key, fallback=True)
 
-        Parameters
-        ----------
-        line: str
-            Line of text to parse
-        """
-        # Strip leading and trailing whitespaces
-        line = line.strip()
+        if key == "sample rate":
+            return self.config.getint("SETTINGS", key, fallback=44100)
 
-        # Skip lines that were just a newline
-        if len(line) < 1:
-            return
-
-        # Get the part of the line before the '='
-        try:
-            index = line.index('=')
-        except ValueError:
-            # '=' not found
-            log(2, "EDITOR", f"Error parsing setting '{line}'.")
-            return
-
-        key = line[:index].strip()
-        value = line[index + 1:].strip()
-
-        try:
-            # Some settings should not be treated as strings
-            if key == "make backups" or key == "sync npc sprites" or key == "fix envelope bug":
-                if value.lower() == "true" or value == "1" or value.lower() == "yes":
-                    value = True
-                else:
-                    value = False
-
-            elif key == "sample rate":
-                value = int(value, 10)
-
-            # Assign the value read from file
-            self._keys[key] = value
-        except KeyError or ValueError:
-            log(2, "EDITOR", f"Invalid setting '{key}' in line '{line}'.")
-
-        return
+        return self.config.get("SETTINGS", key, fallback="")
 
     # ------------------------------------------------------------------------------------------------------------------
 
-    def get(self, key: str) -> any:
-        """
-        Retrieves the value of a setting
-
-        Parameters
-        ----------
-        key: str
-            Name of the setting, e.g. "last rom path"
-
-        Returns
-        -------
-        any
-            The current value for that setting, or None if the key was not valid
-        """
-        try:
-            value = self._keys[key]
-        except KeyError:
-            log(3, "EDITOR", f"Setting '{key}' not found.")
-            return None
-
-        return value
-
-    # ------------------------------------------------------------------------------------------------------------------
-    def set(self, key: str, value: str) -> None:
-        """
-        Changes the value of the given setting
-
-        Parameters
-        ----------
-        key: str
-            The name of the setting to change
-        value: str
-            The desired new value as a string
-        """
-        self._parse_line(f"{key} = {value}")
+    def set(self, key: str, value: Union[int, bool, str]) -> None:
+        self.config.set("SETTINGS", key, value)
